02926b9001d22b6f84eeaaa8a083e20d6d888bff
sawsansalah <sawsan.salaheldin@gmail.com>
2022-10-11 03:00:23 +0200

    Create TwoDimensionArray.py

---
+++ array/TwoDimensionArray.py
@@ -0,0 +1,1 @@
+#To install NumPy on PyCharm, click on File and go to the Settings. Under Settings, choose your Python project and select Python Interpreter. Then, search for the NumPy package and click Install Package
