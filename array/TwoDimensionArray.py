8d59122d133db11279445368a6f7cad0c788e65f
sawsansalah <sawsan.salaheldin@gmail.com>
2022-10-11 03:00:59 +0200

    Update TwoDimensionArray.py

--- array/TwoDimensionArray.py
+++ array/TwoDimensionArray.py
@@ -1,1 +1,2 @@
-#To install NumPy on PyCharm, click on File and go to the Settings. Under Settings, choose your Python project and select Python Interpreter. Then, search for the NumPy package and click Install Package
+'''To install NumPy on PyCharm, click on File and go to the Settings. Under Settings, 
+choose your Python project and select Python Interpreter. Then, search for the NumPy package and click Install Package '''
